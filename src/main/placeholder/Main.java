abb520c8c36b745bfa6fab0598be6d1208fa575d
classy <classy@cs.ubc.ca>
2019-09-08 22:14:27 +0000

    Starter files

---
+++ src/main/placeholder/Main.java
@@ -0,0 +1,7 @@
+package placeholder;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/placeholder/MainTest.java
@@ -0,0 +1,7 @@
+package placeholder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+}
