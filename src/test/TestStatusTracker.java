b516539c452d8683b966721db0225eba2574c832
Tsu Yuan Wang <wang95098@gmail.com>
2019-11-11 18:28:23 -0800

    Added Observer + Network 2

--- src/test/TestStatusTracker.java
+++ src/test/TestStatusTracker.java
@@ -1,25 +1,20 @@
 import main.statustracker.KioskStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import ui.KioskManager;
-
-import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestStatusTracker {
     KioskStatus testKioskStatus;
-    KioskManager testKioskManager;
     @BeforeEach
     public void setup(){
-        testKioskManager = new KioskManager();
         testKioskStatus = new KioskStatus();
     }
 
 
     @Test
     public void setTestKioskStatus(){
-           testKioskStatus.update(testKioskManager, null);
+           testKioskStatus.update(null, null);
            assertEquals(testKioskStatus.getItemsOrdered(),1);
     }
 }
